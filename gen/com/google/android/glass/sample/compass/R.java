5eb5f0d56caffd8816c362ba1923d2c1bf33c72b
Robert Labancz <robert.labancz@genesys.com>
2014-04-27 12:03:06 -0400

    added Property to place object

--- gen/com/google/android/glass/sample/compass/R.java
+++ gen/com/google/android/glass/sample/compass/R.java
@@ -11,13 +11,13 @@ public final class R {
     public static final class array {
         /**  Strings describing locations. 
          */
-        public static final int direction_abbreviations=0x7f080000;
-        public static final int spoken_directions=0x7f080001;
+        public static final int direction_abbreviations=0x7f070000;
+        public static final int spoken_directions=0x7f070001;
     }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int card_margin=0x7f060000;
+        public static final int card_margin=0x7f050000;
     }
     public static final class drawable {
         public static final int gradient50=0x7f020000;
@@ -27,41 +27,38 @@ public final class R {
         public static final int place_mark=0x7f020004;
     }
     public static final class id {
-        public static final int compass=0x7f0b0000;
-        public static final int read_aloud=0x7f0b0003;
-        public static final int stop=0x7f0b0004;
-        public static final int tips_container=0x7f0b0001;
-        public static final int tips_view=0x7f0b0002;
+        public static final int compass=0x7f0a0000;
+        public static final int read_aloud=0x7f0a0003;
+        public static final int stop=0x7f0a0004;
+        public static final int tips_container=0x7f0a0001;
+        public static final int tips_view=0x7f0a0002;
     }
     public static final class layout {
         public static final int compass=0x7f030000;
     }
     public static final class menu {
-        public static final int compass=0x7f0a0000;
-    }
-    public static final class raw {
-        public static final int landmarks=0x7f050000;
+        public static final int compass=0x7f090000;
     }
     public static final class string {
         /**  Launch-related strings. 
          */
-        public static final int app_name=0x7f070000;
-        public static final int magnetic_interference=0x7f070005;
+        public static final int app_name=0x7f060000;
+        public static final int magnetic_interference=0x7f060005;
         /**  Warning messages. 
          */
-        public static final int pitch_too_steep=0x7f070004;
-        public static final int place_text_format=0x7f070003;
+        public static final int pitch_too_steep=0x7f060004;
+        public static final int place_text_format=0x7f060003;
         /**  Menu item strings. 
          */
-        public static final int read_aloud=0x7f070006;
+        public static final int read_aloud=0x7f060006;
         /**  Spoken strings. 
          */
-        public static final int spoken_heading_format=0x7f070001;
-        public static final int spoken_heading_format_one=0x7f070002;
-        public static final int stop=0x7f070007;
+        public static final int spoken_heading_format=0x7f060001;
+        public static final int spoken_heading_format_one=0x7f060002;
+        public static final int stop=0x7f060007;
     }
     public static final class style {
-        public static final int MenuTheme=0x7f090000;
+        public static final int MenuTheme=0x7f080000;
     }
     public static final class xml {
         public static final int compass_show=0x7f040000;
