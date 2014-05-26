a1470486c6f9d95e98d51c9b449349772de9c731
Aaron Surty <aaron.surty@gmail.com>
2014-05-26 13:53:37 -0400

    switching lat/long for police station

--- webapp/police_station.js
+++ webapp/police_station.js
@@ -5,14 +5,14 @@ var g_data;
 
 shapefile.read(__dirname+'/data/police_facilities/Toronto_Police_Facilities_WGS84.shp', function(err, data) {
     g_data = data;
-    debugger;
 });
 
 exports.closest = function(query) {
     var closest_station;
     g_data.features.forEach(function(station) {
         var p = station.geometry.coordinates;
-        var distance = geolib.getDistance(query, {latitude:p[0], longitude:p[1]});
+        console.log(p);
+        var distance = geolib.getDistance(query, {latitude:p[1], longitude:p[0]});
         if(closest_station===undefined || distance < closest_station.distance) {
             closest_station = station.properties;
             closest_station.distance = distance;
